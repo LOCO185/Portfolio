b7dc9f7301fd2b87b851ecd0db50becdba6e85b2
LOCO185 <jloco151@gmail.com>
2022-05-18 17:50:11 +0300

    Navbar added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
-import Preloader from '../src/components/Pre';
-import NavBar from "./components/Navbar";
-import ScrollToTop from "./components/ScrollToTop";
-// import About from './components/About/'
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
+import Resume from "./components/Resume/ResumeNew";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,
+  Navigate
 } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -28,12 +32,12 @@ function App() {
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <NavBar />
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/project" element={<Projects />} /> */}
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
